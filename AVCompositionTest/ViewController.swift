89d9f2e0781cb7e3de80a7742167d251b7d72c32
Yu Kobayashi <sweep.3092@gmail.com>
2020-07-03 02:00:13 +0900

    initial commit

---
+++ AVCompositionTest/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  AVCompositionTest
+//
+//  Created by yu.kobayashi on 2020/07/03.
+//  Copyright © 2020 yu.kobayashi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var playButton: UIButton!
+    var videoPlayer: AVPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let composition = AVMutableComposition()
+        
+        let assetAURL = Bundle.main.url(forResource: "matched_a", withExtension: "mp4")!
+        let assetA = AVURLAsset(url: assetAURL)
+        
+        guard let audioTrackA = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+            debugPrint("Failed to add video track.")
+            return
+        }
+        
+        // 素材Assetの1個目のAudioトラックを使う
+        let srcAudioTrackA = assetA.tracks(withMediaType: .audio)[0]
+        do {
+            // 0秒のタイミングに、映像全体を追加する
+            try audioTrackA.insertTimeRange(srcAudioTrackA.timeRange, of: srcAudioTrackA, at: .zero)
+        } catch let error {
+            debugPrint(error)
+        }
+        
+        let assetBURL = Bundle.main.url(forResource: "matched_b", withExtension: "mp4")!
+        let assetB = AVURLAsset(url: assetBURL)
+
+        guard let audioTrackB = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+            debugPrint("Failed to add video track.")
+            return
+        }
+
+        // 素材Assetの1個目のAudioトラックを使う
+        let srcAudioTrackB = assetB.tracks(withMediaType: .audio)[0]
+        do {
+            // 0秒のタイミングに、映像全体を追加する
+            try audioTrackB.insertTimeRange(srcAudioTrackB.timeRange, of: srcAudioTrackB, at: .zero)
+        } catch let error {
+            debugPrint(error)
+        }
+        
+        // AVPlayerで再生
+        videoPlayer = AVPlayer(playerItem: AVPlayerItem(asset: composition))
+        
+        
+        
+    }
+    @IBAction func playButtonClicked(_ sender: Any) {
+        let playerController = AVPlayerViewController()
+        playerController.player = videoPlayer
+        self.present(playerController, animated: true, completion: {
+            self.videoPlayer.play()
+        })
+    }
+}
+
